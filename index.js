58ecd95cbb5ae8109016be51d072abf21d66b259
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-09 23:44:46 +0300

    refactoring: with white spaces and type of quotes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'usestrict';
-let test1 = "abcdef";
+let test1 = 'abcdef';
 let test2 = 123;
 let test3 = true;
 let test4 = {};
 let test5 = [];
 let test6;
-let test7 = { "abcdef": 123 };
-let test8 = ["abcdef", 123];
-function test9() { return "abcdef" };
+let test7 = { 'abcdef': 123 };
+let test8 = ['abcdef', 123];
+function test9() { return 'abcdef' };
 
 console.log(typeof test1); // string
 console.log(typeof test2); // number
