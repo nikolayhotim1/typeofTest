b60abc02f4dada259a1bde019d8b1872f584f6f9
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-11 15:37:27 +0300

    refactoring: add new tests

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ let test6;
 let test7 = { 'abcdef': 123 };
 let test8 = ['abcdef', 123];
 function test9() { return 'abcdef' };
+let test10 = null;
+let test11 = 0/0; // NaN;
+let test12 = 10 / 0; // Infinity
+let test13 = 10/0 - 10/0; // NaN
 
 console.log(typeof test1); // string
 console.log(typeof test2); // number
@@ -18,3 +22,7 @@ console.log(typeof test6); // undefined
 console.log(typeof test7); // object
 console.log(typeof test8); // object
 console.log(typeof test9); // function
+console.log(typeof test10); // object
+console.log(typeof test11); // number
+console.log(typeof test12); // number
+console.log(typeof test13); // number
